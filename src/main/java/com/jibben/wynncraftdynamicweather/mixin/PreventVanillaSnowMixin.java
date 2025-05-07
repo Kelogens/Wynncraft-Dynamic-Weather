65ca7e66a3f8bff7c47971a27c55ccdbf8bc6011
Kelogens <kelogens@gmail.com>
2025-05-07 04:44:54 +0000

    Update PreventVanillaSnowMixin.java

--- src/main/java/com/jibben/wynncraftdynamicweather/mixin/PreventVanillaSnowMixin.java
+++ src/main/java/com/jibben/wynncraftdynamicweather/mixin/PreventVanillaSnowMixin.java
@@ -2,35 +2,28 @@ package com.jibben.wynncraftdynamicweather.mixin;
 
 import com.jibben.wynncraftdynamicweather.WynncraftDynamicWeather;
 import com.jibben.wynncraftdynamicweather.region.MapRegion;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.WeatherEffectRenderer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(WeatherEffectRenderer.class)
+@Mixin(Biome.class)
 public class PreventVanillaSnowMixin {
     @Unique
-    MapRegion nesaak = new MapRegion(-475, -575, 310, -940, 0.95);
+    private final MapRegion nesaak = new MapRegion(-475, -575, 310, -940, 0.95);
     @Unique
-    MapRegion lusuco = new MapRegion(-475, -575, -85, -295, 0.95);
+    private final MapRegion lusuco = new MapRegion(-475, -575, -85, -295, 0.95);
 
-    @ModifyReturnValue(
-            method = "getPrecipitationAt",
-            at = @At("RETURN")
-    )
-    private Biome.Precipitation modifyPrecipitation(Biome.Precipitation original, Level level, BlockPos pos) {
-        if (Minecraft.getInstance().player != null) {
-            if (!WynncraftDynamicWeather.config.renderVanillaSnow) {
-                if (nesaak.isWithin(pos) || lusuco.isWithin(pos)) {
-                    return Biome.Precipitation.NONE;
-                }
-            }
+    @Inject(method = "getPrecipitationAt", at = @At("RETURN"), cancellable = true)
+    private void modifyPrecipitation(BlockPos pos, int climate, CallbackInfoReturnable<Biome.Precipitation> cir) {
+        if (Minecraft.getInstance().player != null 
+            && !WynncraftDynamicWeather.config.renderVanillaSnow
+            && (nesaak.isWithin(pos) || lusuco.isWithin(pos))) {
+            cir.setReturnValue(Biome.Precipitation.NONE);
         }
-        return original;
     }
 }
