34df55c032323693375d9b75e6ab6b6b6cf7dfe2
Kelogens <kelogens@gmail.com>
2025-05-07 04:45:20 +0000

    Update BiomeMixin.java

--- src/main/java/com/jibben/wynncraftdynamicweather/mixin/BiomeMixin.java
+++ src/main/java/com/jibben/wynncraftdynamicweather/mixin/BiomeMixin.java
@@ -2,43 +2,42 @@ package com.jibben.wynncraftdynamicweather.mixin;
 
 import com.jibben.wynncraftdynamicweather.WynncraftDynamicWeather;
 import com.jibben.wynncraftdynamicweather.region.MapRegion;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Biome.class)
 public class BiomeMixin {
     @Unique
-    MapRegion nesaak = new MapRegion(-475, -575, 310, -940, 0.95);
+    private final MapRegion nesaak = new MapRegion(-475, -575, 310, -940, 0.95);
     @Unique
-    MapRegion lusuco = new MapRegion(-475, -575, -85, -295, 0.95);
+    private final MapRegion lusuco = new MapRegion(-475, -575, -85, -295, 0.95);
     @Unique
-    MapRegion almuj = new MapRegion(850, -2330, 1500, -1180, 0.50);
+    private final MapRegion almuj = new MapRegion(850, -2330, 1500, -1180, 0.50);
 
-    @ModifyReturnValue(method = "getPrecipitationAt", at = @At("RETURN"))
-    private Biome.Precipitation getPrecipitation(Biome.Precipitation original, BlockPos pos) {
-        if ((nesaak.isWithin(pos) || lusuco.isWithin(pos)) && WynncraftDynamicWeather.config.enableMod)  {
-            return Biome.Precipitation.SNOW;
+    @Inject(method = "getPrecipitationAt", at = @At("RETURN"), cancellable = true)
+    private void modifyPrecipitation(BlockPos pos, int climate, CallbackInfoReturnable<Biome.Precipitation> cir) {
+        if (!WynncraftDynamicWeather.config.enableMod) return;
+        
+        if (nesaak.isWithin(pos) || lusuco.isWithin(pos)) {
+            cir.setReturnValue(Biome.Precipitation.SNOW);
+        } else if (almuj.isWithin(pos)) {
+            cir.setReturnValue(Biome.Precipitation.NONE);
         }
-        if (almuj.isWithin(pos) && WynncraftDynamicWeather.config.enableMod)  {
-            return Biome.Precipitation.NONE;
-        }
-        return original;
     }
 
-    @ModifyReturnValue(method = "getBaseTemperature", at = @At("RETURN"))
-    private float getBaseTemperature(float original) {
+    @Inject(method = "getBaseTemperature", at = @At("RETURN"), cancellable = true)
+    private void modifyTemperature(CallbackInfoReturnable<Float> cir) {
         if (Minecraft.getInstance().player != null) {
             BlockPos pos = Minecraft.getInstance().player.blockPosition();
             if (almuj.isWithin(pos) && WynncraftDynamicWeather.config.enableMod) {
-                return 1.0F;
+                cir.setReturnValue(1.0F);
             }
         }
-        return original;
     }
-
 }
