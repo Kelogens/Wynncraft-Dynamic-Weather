89010023a3d3433ec7f388c7edff1f8f9c77234b
Kelogens <kelogens@gmail.com>
2025-05-07 04:48:44 +0000

    Update ForceWeatherMixin.java

--- src/main/java/com/jibben/wynncraftdynamicweather/mixin/ForceWeatherMixin.java
+++ src/main/java/com/jibben/wynncraftdynamicweather/mixin/ForceWeatherMixin.java
@@ -2,40 +2,29 @@ package com.jibben.wynncraftdynamicweather.mixin;
 
 import com.jibben.wynncraftdynamicweather.WynncraftDynamicWeather;
 import com.jibben.wynncraftdynamicweather.config.WeatherType;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Level.class)
 public abstract class ForceWeatherMixin {
-    @ModifyReturnValue(at = @At("RETURN"), method = "getRainLevel")
-    public float changeToRain(float original) {
-        if (!WynncraftDynamicWeather.config.enableMod) {
-            return original;
-        }
-        else if (WynncraftDynamicWeather.getWeatherType() == WeatherType.CLEAR) {
-            return 0F;
-        }
-        else if (WynncraftDynamicWeather.getWeatherType() == WeatherType.RAIN || WynncraftDynamicWeather.getWeatherType() == WeatherType.THUNDER) {
-            return 1.0F;
-        }
-        else return original;
+    @Inject(method = "getRainLevel", at = @At("RETURN"), cancellable = true)
+    private void modifyRainLevel(CallbackInfoReturnable<Float> cir) {
+        if (!WynncraftDynamicWeather.config.enableMod) return;
+        
+        cir.setReturnValue(switch (WynncraftDynamicWeather.getWeatherType()) {
+            case CLEAR -> 0F;
+            case RAIN, THUNDER -> 1.0F;
+            default -> cir.getReturnValue();
+        });
     }
 
-    @ModifyReturnValue(at = @At("RETURN"), method = "getThunderLevel")
-    public float changeToThunder(float original) {
-        if (!WynncraftDynamicWeather.config.enableMod) {
-            return original;
-        }
-        else if (WynncraftDynamicWeather.getWeatherType() == WeatherType.CLEAR) {
-            return 0F;
-        }
-        else if (WynncraftDynamicWeather.getWeatherType() == WeatherType.THUNDER) {
-            return 1.0F;
-        }
-        else return original;
+    @Inject(method = "getThunderLevel", at = @At("RETURN"), cancellable = true)
+    private void modifyThunderLevel(CallbackInfoReturnable<Float> cir) {
+        if (!WynncraftDynamicWeather.config.enableMod) return;
+        
+        cir.setReturnValue(WynncraftDynamicWeather.getWeatherType() == WeatherType.THUNDER ? 1.0F : 0F);
     }
-
-
 }
